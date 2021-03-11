395242dec2d8080e33292d1efcff6bce5619d22a
LivingDead1989 <Steven.Smith1989@Hotmail.co.uk>
2021-03-11 09:52:50 +0000

    +added try except block per def

--- snippets/netmiko/netmiko_combined_tests_try.py
+++ snippets/netmiko/netmiko_combined_tests_try.py
@@ -1,10 +1,10 @@
 import getpass
 from netmiko import ConnectHandler
 
-device_type = input('Enter device type: ')
-device_host = input('Enter host IPv4 address: ')
-device_username = input('Enter Username: ')
-device_password = getpass.getpass(prompt='Enter password: ', stream=None)
+device_type = input('Enter device type (cisco_ios): ')
+device_host = input('Enter host IPv4 address (192.168.56.105): ')
+device_username = input('Enter Username (cisco): ')
+device_password = getpass.getpass(prompt='Enter password (cisco123!): ', stream=None)
 
 testing_device = {
     'device_type': device_type,
@@ -16,64 +16,67 @@ testing_device = {
 
 ## Security Test: Is enable protected by a password?
 def enable_secret():
-    print("Running Security Test: Is enable protected by a password?")
-    connect = ConnectHandler(**testing_device)
-    command = connect.send_command('show running-config | section enable secret')
+    try:
+        print("Running Security Test: Is enable protected by a password?")
+        connect = ConnectHandler(**testing_device)
+        command = connect.send_command('show running-config | section enable secret')
 
-    print(f"I found:\n{command}\n\nResults:")
+        print(f"I found:\n{command}\n\nResults:")
 
-    if command.find("enable secret") != -1:
-        print("Enable password has been configured correctly")
-    else:
-        print("Enable password has not been configured")
+        if command.find("enable secret") != -1:
+            print("Enable password has been configured correctly")
+        else:
+            print("Enable password has not been configured")
+    except:
+        print('There has been an error')
 
 
 ## Security Test: Is SNMPv1 running with a public community string?
 def snmpv1_public():
-    print("Running Security Test: Is SNMPv1 running with a public community string?")
-    connect = ConnectHandler(**testing_device)
-    command = connect.send_command('show running-config | section snmp-server host')
+    try:
+        print("Running Security Test: Is SNMPv1 running with a public community string?")
+        connect = ConnectHandler(**testing_device)
+        command = connect.send_command('show running-config | section snmp-server host')
 
-    command_entries = command.split('\n')
+        command_entries = command.split('\n')
 
-    for entry in command_entries:
-        print(f'I found: {entry}\nResults:')
-        if entry.find(' public ') != -1:
-            print('SNMP has been configured with a community string of "public"')
-            if entry.find('version 2c') != -1:
-                print('SNMP version 2c in use.')
-            elif entry.find('version 3') != -1:
-                print('SNMP version 3 in use.')
+        for entry in command_entries:
+            print(f'I found: {entry}\nResults:')
+            if entry.find(' public ') != -1:
+                print('SNMP has been configured with a community string of "public"')
+                if entry.find('version 2c') != -1:
+                    print('SNMP version 2c in use.')
+                elif entry.find('version 3') != -1:
+                    print('SNMP version 3 in use.')
+                else:
+                    print('SNMP version 1 in use.')
             else:
-                print('SNMP version 1 in use.')
-        else:
-            print('Community string of public is not in use.')
+                print('Community string of public is not in use.')
+    except:
+        print('There has been an error')
 
 
 ## Security Test: Is Telnet enabled?
 def telnet_check():
-    print("Running Security Test: Is Telnet enabled?")
-    connect = ConnectHandler(**testing_device)
-    command = connect.send_command('show running-config | section line vty')
+    try:
+        print("Running Security Test: Is Telnet enabled?")
+        connect = ConnectHandler(**testing_device)
+        command = connect.send_command('show running-config | section line vty')
 
-    print(f"I found:\n{command}\n\nResults:")
+        print(f"I found:\n{command}\n\nResults:")
 
-    if command.find(' telnet') != -1:
-        print('Telnet has been configured!')
-    elif command.find(' all') != -1:
-        print('The All transport method is in use, this includes Telnet!')
-    else:
-        print('Telnet is not in use.')
-
-
-
+        if command.find(' telnet') != -1:
+            print('Telnet has been configured!')
+        elif command.find(' all') != -1:
+            print('The All transport method is in use, this includes Telnet!')
+        else:
+            print('Telnet is not in use.')
+    except:
+        print('There has been an error')
+        
 
 # Main
-#enable_secret()
-#snmpv1_public()
-#telnet_check()
-
-print('Stevens crappy Python security tester\n')
+print('Python security tester\n')
 perform_test = 0
 while perform_test != range(1,3):
     perform_test = int(input(((
